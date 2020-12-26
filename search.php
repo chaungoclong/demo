e459be6f2caaa636514f1180ddaa4702c540d0f7
CHAU NGOC LONG <chaungoclong2411@gmail.com>
2020-12-27 04:07:03 +0700

    edit

--- search.php
+++ search.php
@@ -2,9 +2,6 @@
 require_once 'common.php';
 $keyWord = input_get("q");
 $result = fetch_list("db_product", "pro_name LIKE('%$keyWord%')", ["*"], 1);
-echo "ok";
-echo "okk";
-echo "good number one";
 ?>
 <!DOCTYPE html>
 <html>
